1da741027c69ac4c39e13bf3d3429a57293ecb00
pedro <pedrolelis@hotmail.com>
2022-03-07 01:19:14 -0300

    first commit

---
+++ agenda/Models/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace agenda.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
---
+++ agenda/Models/Usuario.cs
@@ -0,0 +1,12 @@
+﻿namespace agenda.Models
+{
+    public class Usuario
+    {
+        public Usuario() { }
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public string Telefone { get; set; }
+
+    }
+}
